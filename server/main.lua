87cbd5b086e210f874ec0e67f7e94afc59e17d1c
JGUsman <58899378+JGUsman007@users.noreply.github.com>
2023-04-12 04:26:40 +0500

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -4,3 +4,9 @@ RegisterNetEvent('JG-Sharedgarage:spawnvehicle',function()
     TriggerClientEvent('JG-Sharedgarage:setvehicleout',-1, function()
     end)
 end)
+
+
+RegisterNetEvent('JG-Sharedgarage:despawnvehicle',function()
+    TriggerClientEvent('JG-Sharedgarage:setvehiclein',-1, function()
+    end)
+end)
