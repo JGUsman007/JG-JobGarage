03fdc8325cf10f9bbc6617bf1411f9bf78b540c4
JGUsman <58899378+JGUsman007@users.noreply.github.com>
2023-06-18 05:50:20 +0500

    Update main.lua
    Update garage on player joined

--- server/main.lua
+++ server/main.lua
@@ -1,12 +1,23 @@
 ESX = exports['es_extended']:getSharedObject()
 
-RegisterNetEvent('JG-Sharedgarage:spawnvehicle',function()
-    TriggerClientEvent('JG-Sharedgarage:setvehicleout',-1, function()
-    end)
+
+
+RegisterNetEvent('JG-Sharedgarage:spawnvehicle',function(vehplate)
+    TriggerClientEvent('JG-Sharedgarage:setvehicleout',-1,vehplate)
 end)
 
 
-RegisterNetEvent('JG-Sharedgarage:despawnvehicle',function()
-    TriggerClientEvent('JG-Sharedgarage:setvehiclein',-1, function()
-    end)
+RegisterNetEvent('JG-Sharedgarage:savevehicle',function(String,vehicle)
+    TriggerClientEvent('JG-Sharedgarage:savevehicle',-1,String,vehicle)
+end)
+
+
+
+RegisterNetEvent('update:garage', function (data)
+    _Garage = data
+end)
+
+
+lib.callback.register('Get:Garage', function()
+    return _Garage
 end)
